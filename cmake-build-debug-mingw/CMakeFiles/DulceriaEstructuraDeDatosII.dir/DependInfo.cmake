a8bd15035b1e9335ddfd0140f9b8382262747cea
Maximiliano Fonseca <maxfonseca.r@gmail.com>
2019-11-09 21:01:39 -0600

    PRUEBA

---
+++ cmake-build-debug-mingw/CMakeFiles/DulceriaEstructuraDeDatosII.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "A:/Ninefrm/Documents/GitKraken/DulceriaEstructuraDeDatosII/FuncionesOrdenesDeCompra.cpp" "A:/Ninefrm/Documents/GitKraken/DulceriaEstructuraDeDatosII/cmake-build-debug-mingw/CMakeFiles/DulceriaEstructuraDeDatosII.dir/FuncionesOrdenesDeCompra.cpp.obj"
+  "A:/Ninefrm/Documents/GitKraken/DulceriaEstructuraDeDatosII/OrdenesDeCompraLF.cpp" "A:/Ninefrm/Documents/GitKraken/DulceriaEstructuraDeDatosII/cmake-build-debug-mingw/CMakeFiles/DulceriaEstructuraDeDatosII.dir/OrdenesDeCompraLF.cpp.obj"
+  "A:/Ninefrm/Documents/GitKraken/DulceriaEstructuraDeDatosII/ProductoLV.cpp" "A:/Ninefrm/Documents/GitKraken/DulceriaEstructuraDeDatosII/cmake-build-debug-mingw/CMakeFiles/DulceriaEstructuraDeDatosII.dir/ProductoLV.cpp.obj"
+  "A:/Ninefrm/Documents/GitKraken/DulceriaEstructuraDeDatosII/ProveedorCD.cpp" "A:/Ninefrm/Documents/GitKraken/DulceriaEstructuraDeDatosII/cmake-build-debug-mingw/CMakeFiles/DulceriaEstructuraDeDatosII.dir/ProveedorCD.cpp.obj"
+  "A:/Ninefrm/Documents/GitKraken/DulceriaEstructuraDeDatosII/Prueba.cpp" "A:/Ninefrm/Documents/GitKraken/DulceriaEstructuraDeDatosII/cmake-build-debug-mingw/CMakeFiles/DulceriaEstructuraDeDatosII.dir/Prueba.cpp.obj"
+  "A:/Ninefrm/Documents/GitKraken/DulceriaEstructuraDeDatosII/main.cpp" "A:/Ninefrm/Documents/GitKraken/DulceriaEstructuraDeDatosII/cmake-build-debug-mingw/CMakeFiles/DulceriaEstructuraDeDatosII.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug-mingw/CMakeFiles/DulceriaEstructuraDeDatosII.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DulceriaEstructuraDeDatosII.dir/FuncionesOrdenesDeCompra.cpp.obj"
+  "CMakeFiles/DulceriaEstructuraDeDatosII.dir/OrdenesDeCompraLF.cpp.obj"
+  "CMakeFiles/DulceriaEstructuraDeDatosII.dir/ProductoLV.cpp.obj"
+  "CMakeFiles/DulceriaEstructuraDeDatosII.dir/ProveedorCD.cpp.obj"
+  "CMakeFiles/DulceriaEstructuraDeDatosII.dir/Prueba.cpp.obj"
+  "CMakeFiles/DulceriaEstructuraDeDatosII.dir/main.cpp.obj"
+  "DulceriaEstructuraDeDatosII.exe"
+  "DulceriaEstructuraDeDatosII.exe.manifest"
+  "DulceriaEstructuraDeDatosII.pdb"
+  "libDulceriaEstructuraDeDatosII.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DulceriaEstructuraDeDatosII.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
